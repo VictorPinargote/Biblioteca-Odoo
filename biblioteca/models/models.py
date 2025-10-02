faa163db333182a87a5377c0fb2599cc8063aef3
VictorPinargote <joelpinargote76@gmail.com>
2025-10-02 10:05:00 -0500

    commit 2

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class biblioteca(models.Model):
-#     _name = 'biblioteca.biblioteca'
-#     _description = 'biblioteca.biblioteca'
+class biblioteca_libro(models.Model):
+    _name = 'biblioteca.libro'
+    _description = 'biblioteca.biblioteca'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True) #---- Se guarda en la base de datos 
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
 
