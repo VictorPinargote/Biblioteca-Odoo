b7fa3227c2cda4c5b1573654dfa0666356f3fd36
VictorPinargote <joelpinargote76@gmail.com>
2025-10-08 10:02:09 -0500

    commit 5

--- biblioteca/__manifest__.py
+++ biblioteca/__manifest__.py
@@ -30,10 +30,4 @@ Long description of module's purpose
     #'demo': [
      #   'demo/demo.xml',
     #],
-    'application': True,
-    'license': 'AGPL-3'
 }
-
-#12.5.0 Nos indica que cambio ha tenido el software, el tercero, son cosas pequeñas tipo errores ortograficos, errores que habia, el segundo es el cambio de la lógica, y el primero es un cambio mucho mas robuzto que cambia toda la estructura.*/
-#Depende de donde este el cambio, se hacen los test, si el tercero lo actualizo nomas, el segundo depende pero no hay que ser muchas pruebas el primero si o si toca hacer pruebas robustas*/
-
